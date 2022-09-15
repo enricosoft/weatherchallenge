764e4a35a91d854e44905491e521b92e096fa847
Enrico Piccini <epiccini@onit.it>
2022-09-15 21:23:27 +0200

    Completed the CLI application

--- WeatherChallengeCli/Program.cs
+++ WeatherChallengeCli/Program.cs
@@ -11,19 +11,33 @@ namespace WeatherChallengeCli
             var _citiesService = new Cities(settings["AppSettings:MusementBaseApiUrl"]);
             var _weatherService = new Weather(settings["AppSettings:WeatherApiKey"]);
 
-            /* TODO
-            var citiesList = _citiesService.Get();
-            _weatherService.Get(new Weather.ForecastRequest()
+            var citiesList = _citiesService.Get().GetAwaiter().GetResult();
+            if (citiesList != null)
             {
-                coords = new Weather.ForecastRequest.Coords() {
-                    Latitude = 0,
-                    Longitude = 0
-                },
-                days = 2
-            }).GetAwaiter()
-              .GetResult();
-            */
+                foreach (var city in citiesList.Cities)
+                {
+                    var forecasts = _weatherService.Get(new ForecastRequest()
+                    {
+                        coords = new ForecastRequest.Coords()
+                        {
+                            Latitude = city.latitude,
+                            Longitude = city.longitude
+                        },
+                        days = 2
+                    }).GetAwaiter().GetResult();
 
+                    string forecastDay1 = forecasts!.forecast.forecastday[0].day.condition.text;
+                    string forecastDay2 = forecasts!.forecast.forecastday[1].day.condition.text;
+
+                    Console.WriteLine($"Processed city {city.name} | {forecastDay1} - {forecastDay2}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error retrieving cities");
+            }
+
+            Console.WriteLine("-- Press a key to exit --");
             Console.ReadKey();
         }
     }
